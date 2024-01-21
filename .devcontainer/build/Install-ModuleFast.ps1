23c25c9cebd8d77e3b2941e8de59259f11c0cd82
sharedstuff <136310614+sharedstuff@users.noreply.github.com>
2024-01-21 19:26:02 +0000

    changed: BuildProcess / updated: ModuleFast

--- .devcontainer/build/Install-ModuleFast.ps1
+++ .devcontainer/build/Install-ModuleFast.ps1
@@ -1,20 +1,13 @@
-if ($env:PowerShell_InstallModuleFast) {
-    # Install Modules using Install-ModuleFast
-    $Modules = $env:PowerShell_InstallModuleFast | Invoke-Expression
-}
-else {
-    $Modules = @()
-}
-
-$Modules | ConvertTo-Json
-
 # Create ModuleFolder
 $ModuleFolder = $env:PowerShell_ModuleFolder
 if (-not (Test-Path $ModuleFolder)) { New-Item $ModuleFolder -ItemType Directory }
 
 # Load ModuleFast on-the-fly
 Invoke-WebRequest 'bit.ly/modulefast' | Invoke-Expression
-
-# Install-Module(s)
 Install-ModuleFast ModuleFast -Destination $ModuleFolder -Verbose
-Install-ModuleFast $Modules -Destination $ModuleFolder -Verbose
+
+# Install Modules using Install-ModuleFast
+if ($env:PowerShell_InstallModuleFast) {
+    $Modules = $env:PowerShell_InstallModuleFast | Invoke-Expression
+    Install-ModuleFast $Modules -Destination $ModuleFolder -Verbose
+}
